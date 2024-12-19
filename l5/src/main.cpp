0dfba91709b67dbcd24a17ed119a1c2a2ed067cf
vitosotdihaet <vitostikitos@gmail.com>
2024-12-19 14:07:40 +0300

    [l5] Upload a report

--- l5/src/main.cpp
+++ l5/src/main.cpp
@@ -5,27 +5,30 @@
 
 
 
-const int SAMPLES_PER_PIXEL = 32;
+const int SAMPLES_PER_PIXEL = 512;
 
-const int width = 800;
-const int height = 600;
+constexpr int FACTOR = 100;
+
+const int width = 4 * FACTOR;
+const int height = 3 * FACTOR;
 
 const glm::vec3 BACKGROUND_COLOR = glm::vec3(0.2, 0.2, 0.3);
 const glm::vec3 FLOOR_COLOR = glm::vec3(0.3, 0.3, 0.3);
 
 const glm::vec3 sphereColor1 = glm::vec3(1.0, 0.0, 0.0);
 const float sphereRadius1 = 1.0;
-const glm::vec3 spherePosition1 = glm::vec3(1, 1, -5);
+const glm::vec3 spherePosition1 = glm::vec3(0.25, 1, -5);
 
 const glm::vec3 sphereColor2 = glm::vec3(0.0, 0.5, 1);
 const float sphereRadius2 = 0.75;
-const glm::vec3 spherePosition2 = glm::vec3(-3.5, 1.5, -4);
+const glm::vec3 spherePosition2 = glm::vec3(-0.25, 3, -3.5);
 
 const float floorY = 0.0;
 
-const glm::vec3 cameraPos(0, 1, 0);
+float cameraFov = glm::radians(60.0f);
+const glm::vec3 cameraPos(0, 1, 2);
 
-glm::vec3 lightPosition = glm::vec3(-2, 5, -3);
+glm::vec3 lightPosition = glm::vec3(-2, 5, -2);
 glm::vec3 lightColor = glm::vec3(1.0, 1.0, 1.0);
 float lightRadius;
 
@@ -71,7 +74,7 @@ float calculateShadow(const glm::vec3 &point) {
         const glm::vec3 jitteredLightDirection = glm::normalize(jitteredLightPosition - point);
 
         float _;
-        if (
+        if ( // 1e-3f is to prevent from self-shadowing
             intersectSphere(point + jitteredLightDirection * 1e-3f, jitteredLightDirection, _, spherePosition1, sphereRadius1) || 
             intersectSphere(point + jitteredLightDirection * 1e-3f, jitteredLightDirection, _, spherePosition2, sphereRadius2) || 
             intersectFloor(point + jitteredLightDirection * 1e-3f, jitteredLightDirection, _)
@@ -88,39 +91,35 @@ glm::vec3 traceRay(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection) {
 
     glm::vec3 outputColor = BACKGROUND_COLOR;
 
-    bool hitFloor = intersectFloor(rayOrigin, rayDirection, lightDistanceFloor);
-    if (hitFloor) {
+    if (intersectFloor(rayOrigin, rayDirection, lightDistanceFloor)) {
         const glm::vec3 point = rayOrigin + lightDistanceFloor * rayDirection;
         const glm::vec3 normal = glm::vec3(0, 1, 0);
         const glm::vec3 lightDirection = normalize(lightPosition - point);
 
-        float antiShadowFactor = 1.0f - calculateShadow(point);
         float intensity = std::max(dot(normal, lightDirection), 0.0f);
-        glm::vec3 floorColor = FLOOR_COLOR * lightColor * intensity * antiShadowFactor;
+        float antiShadowFactor = 1.0f - calculateShadow(point);
 
-        outputColor = floorColor;
+        outputColor = FLOOR_COLOR * lightColor * intensity * antiShadowFactor;
     }
 
-    bool hitSphere1 = intersectSphere(rayOrigin, rayDirection, lightDistanceSphere1, spherePosition1, sphereRadius1);
-    if (hitSphere1) {
+    if (intersectSphere(rayOrigin, rayDirection, lightDistanceSphere1, spherePosition1, sphereRadius1)) {
         const glm::vec3 point = rayOrigin + lightDistanceSphere1 * rayDirection;
         const glm::vec3 normal = glm::normalize(point - spherePosition1);
         const glm::vec3 lightDirection = glm::normalize(lightPosition - point);
 
-        float antiShadowFactor = 1.0f - calculateShadow(point);
         float intensity = std::max(dot(normal, lightDirection), 0.0f);
+        float antiShadowFactor = 1.0f - calculateShadow(point);
 
         outputColor = sphereColor1 * lightColor * intensity * antiShadowFactor;
     }
 
-    bool hitSphere2 = intersectSphere(rayOrigin, rayDirection, lightDistanceSphere2, spherePosition2, sphereRadius2);
-    if (hitSphere2) {
+    if (intersectSphere(rayOrigin, rayDirection, lightDistanceSphere2, spherePosition2, sphereRadius2)) {
         const glm::vec3 point = rayOrigin + lightDistanceSphere2 * rayDirection;
         const glm::vec3 normal = glm::normalize(point - spherePosition2);
         const glm::vec3 lightDirection = glm::normalize(lightPosition - point);
 
-        float antiShadowFactor = 1.0f - calculateShadow(point);
         float intensity = std::max(dot(normal, lightDirection), 0.0f);
+        float antiShadowFactor = 1.0f - calculateShadow(point);
 
         outputColor = sphereColor2 * lightColor * intensity * antiShadowFactor;
     }
@@ -135,9 +134,8 @@ int main() {
 
     std::cout << "P3\n" << width << " " << height << "\n255\n";
 
-    float fov = glm::radians(90.0f);
     float aspectRatio = float(width) / height;
-    float scale = tan(fov / 2);
+    float scale = tan(cameraFov / 2);
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
@@ -147,11 +145,9 @@ int main() {
 
             glm::vec3 color = traceRay(cameraPos, rayDirection);
 
-            std::cout << static_cast<int>(255 * std::clamp(color.r, 0.0f, 1.0f)) << " "
-                      << static_cast<int>(255 * std::clamp(color.g, 0.0f, 1.0f)) << " "
-                      << static_cast<int>(255 * std::clamp(color.b, 0.0f, 1.0f)) << "\n";
+            std::cout << static_cast<int>(255 * std::clamp(color.r, 0.0f, 1.0f)) << ' '
+                      << static_cast<int>(255 * std::clamp(color.g, 0.0f, 1.0f)) << ' '
+                      << static_cast<int>(255 * std::clamp(color.b, 0.0f, 1.0f)) << '\n';
         }
     }
-
-    return 0;
 }
